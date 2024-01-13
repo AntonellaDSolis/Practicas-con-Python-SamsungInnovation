35a3cf170a1a49572af40d6fb046a0359b3ca4ce
AntonellaDSolis <125618828+AntonellaDSolis@users.noreply.github.com>
2024-01-13 12:48:17 -0300

    Delete TP 1 directory

--- TP 1/Ejercicio 10.py
+++
@@ -1,9 +0,0 @@
-# Ejercicio 10
-
-sumatoria : int = 0
-numero : int = 1
-
-while numero != 0:
-    numero = int(input("Ingrese un número: "))
-    sumatoria = sumatoria + numero
-print(sumatoria)
--- TP 1/Ejercicio 3.py
+++
@@ -1,8 +0,0 @@
-# Ejercicio 3
-
-numero = int(input("Ingrese un número entero: "))
-
-if numero % 2 == 0:
-    print("Es un número par")
-else:
-    print("Es un número impar")
